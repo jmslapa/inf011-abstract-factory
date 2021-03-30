7c34534ba4e8786f9f9601c427e5687e7c24497b
João Manuel dos Santos Lapa <joaomanoel1996@hotmail.com>
2021-03-29 22:13:32 -0300

    Criada interface para o LanguageService

---
+++ support/Contracts/Services/LanguageServiceContract.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Support\Contracts\Services;
+
+use Support\Contracts\Models\LanguageToolkitContract;
+
+interface LanguageServiceContract
+{
+    public function getLanguageToolKit(string $language) : LanguageToolkitContract;
+
+    public function getSupportedLanguages() : array;
+
+    public function highlight(object $file) : object;
+
+    public function compile(string $fileName,  string $fileContent): string;
+}
