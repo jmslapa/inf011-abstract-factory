e8163840f1b373954447443f4366ab06d3df5a6c
João Manuel dos Santos Lapa <joaomanoel1996@hotmail.com>
2021-04-18 00:04:42 -0300

    Adicionado Container como dependência de Application. Criados métodos registerBindings, registerLanguageToolkits e registerHelpers, sendo todos incluidos na sobrescrita do método boot.

--- bootstrap/Application.php
+++ bootstrap/Application.php
@@ -2,13 +2,56 @@
 
 namespace Bootstrap;
 
+use ReflectionClass;
+use App\Services\LanguageService;
+use Support\Singletons\Container;
 use Mf\Bootstrap\Application as BootstrapApplication;
+use Support\Abstracts\Factory\AbstractLanguageToolkitFactory;
 
 class Application extends BootstrapApplication
 {
+    private Container $container;
+
     protected function loadRoutes(): array
     {
         $routes  = require src('/routes/web.php');
         return $routes;
+    }
+
+    protected function boot()
+    {
+        $this->container = Container::getInstance();
+        $this->registerHelpers();
+        $this->registerBindings();
+        $this->registerLanguageToolkits();
+        parent::boot();
+    }
+
+    private function registerBindings()
+    {
+        $this->container->bind('languageService', new LanguageService);
+    }
+
+    private function registerLanguageToolkits()
+    {
+        $dirs = preg_grep('/^([^.])/', scandir(src('plugins/Lang/Toolkits')));
+        $suffix = 'ToolkitFactory';
+        $supportedLangsKeyName = 'supportedLangs';
+
+        $this->container->bind($supportedLangsKeyName, []);
+        foreach ($dirs as $lang) {
+            $parentClass = AbstractLanguageToolkitFactory::class;
+            $class = "\\Plugins\\Lang\\Toolkits\\$lang\\Factory\\LanguageToolkitFactory";
+            if (class_exists($class) && (new ReflectionClass($class))->isSubclassOf($parentClass)) {
+                $supportedLangs = $this->container->resolve($supportedLangsKeyName);
+                $this->container->bind(lcfirst($lang.$suffix), $class::getInstance());
+                $this->container->bind($supportedLangsKeyName, [...$supportedLangs, strtolower($lang)]);
+            }
+        }
+    }
+
+    private function registerHelpers()
+    {
+        require_once 'helpers.php';
     }
 }
