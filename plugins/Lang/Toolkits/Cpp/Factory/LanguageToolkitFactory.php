614c0ce47e4aeeb0484b0f4044f48827196e3379
João Manuel dos Santos Lapa <joaomanoel1996@hotmail.com>
2021-04-17 23:54:15 -0300

    As LanguageToolkitFactories dos plugins agora estendem AbstractLanguageToolkitFactory

--- plugins/Lang/Toolkits/Cpp/Factory/LanguageToolkitFactory.php
+++ plugins/Lang/Toolkits/Cpp/Factory/LanguageToolkitFactory.php
@@ -3,27 +3,27 @@
 namespace Plugins\Lang\Toolkits\Cpp\Factory;
 
 use App\Models\Lang\Toolkits\LanguageToolkit;
-use Support\Contracts\Factories\LanguageToolkitFactoryContract;
 use Support\Contracts\Models\CompilerContract;
 use Support\Contracts\Models\HighlighterContract;
 use Support\Contracts\Models\LanguageToolkitContract;
 use Plugins\Lang\Toolkits\Cpp\Models\Compiler;
 use Plugins\Lang\Toolkits\Cpp\Models\Highlighter;
+use Support\Abstracts\Factory\AbstractLanguageToolkitFactory;
 
-class LanguageToolkitFactory implements LanguageToolkitFactoryContract
+class LanguageToolkitFactory extends AbstractLanguageToolkitFactory
 {
-    public static function makeHighlighter(): HighlighterContract
+    public function makeHighlighter(): HighlighterContract
     {
         return new Highlighter;
     }
 
-    public static function makeCompiler(): CompilerContract
+    public function makeCompiler(): CompilerContract
     {
         return new Compiler;
     }
 
-    public static function makeToolkit(): LanguageToolkitContract
+    public function makeToolkit(): LanguageToolkitContract
     {
-        return new LanguageToolkit(self::makeHighlighter(), self::makeCompiler());
+        return new LanguageToolkit($this->makeHighlighter(), $this->makeCompiler());
     }
 }
